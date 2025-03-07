052337f97655bd8e2211ec1cc9bd1e4437ee6b4f
AyushChopra24 <ayushchopra_24@yahoo.com>
2025-03-06 21:44:24 -0500

    Successful Server Creation

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require("express");
 const mustacheExpress = require("mustache-express");
 const bodyParser = require("body-parser");
+const workoutRoutes = require("./routes/workoutRoutes");
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", "./views");
+
+app.use("/", workoutRoutes);
+
+app.listen(PORT, ()=> {console.log('Server Successfully Running')});
